e2fd7b96be0f685f0bfb044f2981b68f1e534b3d
dmesa <dm31284@gmail.com>
2024-06-27 09:14:46 -0500

    feat/dm/HU_Crear tipos documentales de proceso

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/see-documental-types/see-documental-types.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/see-documental-types/see-documental-types.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActionType } from '../../components/dialog/models/action-type.enum';
+import { DialogAction } from '../../components/dialog/models/dialog-action';
+import { DialogData } from '../../components/dialog/models/dialog-data';
+import { DialogType } from '../../components/dialog/models/dialog-type';
+import { DialogService } from '../../components/dialog/services/dialog.service';
 
 interface DocumentType {
   id: number;
@@ -12,6 +18,9 @@ interface DocumentType {
   styleUrl: './see-documental-types.component.css',
 })
 export class SeeDocumentalTypesComponent {
+  @ViewChild('dialog', { read: ViewContainerRef }) dialog!: ViewContainerRef;
+  subscription!: Subscription;
+
   tiposDocumentales: DocumentType[] = [
     {
       id: 1,
@@ -34,4 +43,44 @@ export class SeeDocumentalTypesComponent {
       documents: ['Acta de liquidación', 'Acta de apertura', 'Acta de cierre'],
     },
   ];
+
+  constructor(private dialogService: DialogService) {}
+
+  // MODALES
+  showSaveInformationModal(proccess: DocumentType[], $event: Event) {
+    $event.preventDefault();
+
+    const dialogData = new DialogData();
+    dialogData.title = 'Guardar información';
+    dialogData.body = `¿Está seguro de esta información?`;
+    dialogData.textButtonCancel = 'Cancelar';
+    dialogData.type = DialogType.warning;
+
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        if (dialogAction.action === ActionType.confirm) {
+          const body = `El registro fue guardado con éxito`;
+          dialogAction.eventClose.emit();
+          this.showSuccessTaskInitializationAlertState(body);
+        } else {
+          dialogAction.eventClose.emit();
+        }
+      });
+  }
+
+  showSuccessTaskInitializationAlertState(body: string) {
+    const dialogData = new DialogData();
+    dialogData.title = body;
+    dialogData.type = DialogType.success;
+    dialogData.buttonConfirm = false;
+    dialogData.textButtonCancel = 'Cerrar';
+
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        dialogAction.eventClose.emit();
+        location.reload();
+      });
+  }
 }
