3d304ebc04b99ce4afc27db0e282080601980b25
Zheone9 <jorgito.2703@gmail.com>
2024-06-28 14:31:39 -0500

    se añade la hu de la carpeta 17. configurar flujo etapas

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/app.routes.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/app.routes.ts
@@ -15,6 +15,7 @@ import { DocumentObservationsComponent } from './pages/document-observations/doc
 import { ManageTaskComponent } from './pages/manage-task/manage-task.component';
 import { SetStageRequirementsComponent } from './pages/set-stage-requirements/set-stage-requirements.component';
 import { CreateTaskComponent } from './pages/createTask/createTask.component';
+import { ConfigureFlowStagesComponent } from './pages/configure-flow-stages/configure-flow-stages.component';
 
 export const appRoutes: Route[] = [
 
@@ -37,8 +38,6 @@ export const appRoutes: Route[] = [
   { path: 'consultar-alertas',component: AlertsByResponsibleComponent},
   { path: 'configurar-requerimientos-etapa',component: SetStageRequirementsComponent},
   { path: 'crear-tarea',component: CreateTaskComponent},
-
-
-
+  { path: 'configurar-flujo-etapas',component: ConfigureFlowStagesComponent},
 
 ];
