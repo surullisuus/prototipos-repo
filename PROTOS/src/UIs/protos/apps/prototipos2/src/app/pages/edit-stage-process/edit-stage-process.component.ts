b6eb49fe63423f2660a0c60723dcd62bad1449e9
ManuelLV <manlopez@unicauca.edu.co>
2024-07-02 13:31:25 -0500

    correcciones

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-stage-process/edit-stage-process.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-stage-process/edit-stage-process.component.ts
@@ -1,8 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActionType } from '../../components/dialog/models/action-type.enum';
+import { DialogAction } from '../../components/dialog/models/dialog-action';
+import { DialogData } from '../../components/dialog/models/dialog-data';
+import { DialogType } from '../../components/dialog/models/dialog-type';
+import { DialogService } from '../../components/dialog/services/dialog.service';
 
 @Component({
   selector: 'app-edit-stage-process',
   templateUrl: './edit-stage-process.component.html',
   styleUrl: './edit-stage-process.component.css',
 })
-export class EditStageProcessComponent {}
+export class EditStageProcessComponent {
+  @ViewChild('dialog', { read: ViewContainerRef }) dialog!: ViewContainerRef;
+  constructor(private dialogService: DialogService) {}
+  isDisabledSubmit = false;
+  subscription!: Subscription;
+  SaveModal() {
+    const dialogData = new DialogData();
+    dialogData.title = "¿Está seguro de guardar cambios?";
+    dialogData.textButtonCancel = "Cerrar";
+    dialogData.textButtonConfirm = "Aceptar";
+    dialogData.type = DialogType.warning;
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        if (dialogAction.action === ActionType.confirm) {
+          dialogAction.eventClose.emit();
+          this.onSavedModal(); 
+          // Aquí puedes agregar el setTimeout si es necesario
+        } else {
+          dialogAction.eventClose.emit();
+        }
+      });
+  }
+
+  onSavedModal(){
+    const dialogData = new DialogData();
+    dialogData.title = "Cambios guardados de forma exitosa";
+    dialogData.buttonConfirm = false;
+    dialogData.textButtonCancel = 'Cerrar';
+    dialogData.type = DialogType.success;
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((DialogAction: DialogAction) => {
+          DialogAction.eventClose.emit()
+          location.reload();
+      })
+  }
+  onCloseModal() {}
+}
