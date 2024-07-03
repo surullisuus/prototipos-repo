518d0017114f9d35207a2b45139d98d30d270556
dmesa <dm31284@gmail.com>
2024-07-03 10:51:40 -0500

    feat/dm/FIX toogle buttons && titles HU ConfigurarEtapaTarea

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-task/edit-task.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-task/edit-task.component.ts
@@ -9,6 +9,11 @@ import { ActivatedRoute } from '@angular/router';
 export class EditTaskComponent {
   task?: any;
   loading = true;
+  ableTaskDays = false;
+  ableTaskHours = false;
+
+  ableAlertDays = false;
+  ableAlertHours = false;
 
   constructor(private _route: ActivatedRoute) {
     _route.params.subscribe((params) => {
@@ -18,5 +23,41 @@ export class EditTaskComponent {
     setTimeout(() => {
       this.loading = false;
     }, 1000);
+  }
+
+  onAbleTaskDays() {
+    this.ableTaskDays = !this.ableTaskDays;
+    this.ableTaskHours = false;
+  }
+
+  onAbleTaskHours() {
+    this.ableTaskDays = false;
+    this.ableTaskHours = !this.ableTaskHours;
+  }
+
+  onAbleAlertDays() {
+    this.ableAlertDays = !this.ableAlertDays;
+    this.ableAlertHours = false;
+  }
+
+  onAbleAlertHours() {
+    this.ableAlertDays = false;
+    this.ableAlertHours = !this.ableAlertHours;
+  }
+
+  get taskDays() {
+    return this.ableTaskDays;
+  }
+
+  get taskHours() {
+    return this.ableTaskHours;
+  }
+
+  get alertDays() {
+    return this.ableAlertDays;
+  }
+
+  get alertHours() {
+    return this.ableAlertHours;
   }
 }
