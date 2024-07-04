0004a01c12d8b9f7cdd337b9d5925c606dd3fec5
dmesa <dm31284@gmail.com>
2024-07-04 14:34:38 -0500

    fixnames

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-email-template/edit-email-template.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/edit-email-template/edit-email-template.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
@@ -21,15 +20,6 @@ interface CurrentProcess {
   cc: string;
   desde: string;
 }
-=======
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ActionType } from '../../components/dialog/models/action-type.enum';
-import { DialogAction } from '../../components/dialog/models/dialog-action';
-import { DialogData } from '../../components/dialog/models/dialog-data';
-import { DialogType } from '../../components/dialog/models/dialog-type';
-import { DialogService } from '../../components/dialog/services/dialog.service';
->>>>>>> origin/main
 
 @Component({
   selector: 'app-edit-email-template',
@@ -37,7 +27,6 @@ import { DialogService } from '../../components/dialog/services/dialog.service';
   styleUrl: './edit-email-template.component.css',
 })
 export class EditEmailTemplateComponent {
-<<<<<<< HEAD
   selectedProcess: string | null = null;
   selectedDocument: string | null = null;
   currentProcessId: CurrentProcess = {
@@ -144,43 +133,4 @@ export class EditEmailTemplateComponent {
   setSelectedDocument(document: { id: number; text: string }) {
     this.selectedDocument = document.text;
   }
-=======
-  @ViewChild('dialog', { read: ViewContainerRef }) dialog!: ViewContainerRef;
-  constructor(private dialogService: DialogService) {}
-  isDisabledSubmit = false;
-  subscription!: Subscription;
-  SaveModal() {
-    const dialogData = new DialogData();
-    dialogData.title = "¿Está seguro de guardar cambios?";
-    dialogData.textButtonCancel = "Cerrar";
-    dialogData.textButtonConfirm = "Aceptar";
-    dialogData.type = DialogType.warning;
-    this.subscription = this.dialogService
-      .openModal(this.dialog, dialogData)
-      .subscribe((dialogAction: DialogAction) => {
-        if (dialogAction.action === ActionType.confirm) {
-          dialogAction.eventClose.emit();
-          this.onSavedModal(); 
-          // Aquí puedes agregar el setTimeout si es necesario
-        } else {
-          dialogAction.eventClose.emit();
-        }
-      });
-  }
-
-  onSavedModal(){
-    const dialogData = new DialogData();
-    dialogData.title = "Cambios guardados de forma exitosa";
-    dialogData.buttonConfirm = false;
-    dialogData.textButtonCancel = 'Cerrar';
-    dialogData.type = DialogType.success;
-    this.subscription = this.dialogService
-      .openModal(this.dialog, dialogData)
-      .subscribe((DialogAction: DialogAction) => {
-          DialogAction.eventClose.emit()
-          location.reload();
-      })
-  }
-  onCloseModal() {}
->>>>>>> origin/main
 }
