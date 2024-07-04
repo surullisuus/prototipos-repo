a025438cc430793e354c8ce9b9527cc694bb55d5
dmesa <dm31284@gmail.com>
2024-07-04 14:30:44 -0500

    feat/dm/HU - Eliminar plantillas correo electrónico tipos documentales

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/manage-plantillas-correo/manage-plantillas-correos.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/manage-plantillas-correo/manage-plantillas-correos.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { DialogService } from '../../components/dialog/services/dialog.service';
+import { DialogData } from '../../components/dialog/models/dialog-data';
+import { DialogType } from '../../components/dialog/models/dialog-type';
+import { ActionType } from '../../components/dialog/models/action-type.enum';
+import { DialogAction } from '../../components/dialog/models/dialog-action';
 
 interface Process {
   id: number;
@@ -14,6 +20,9 @@ interface Process {
   styleUrl: './manage-plantillas-correos.component.css',
 })
 export class ManagePlantillasCorreosComponent {
+  @ViewChild('dialog', { read: ViewContainerRef }) dialog!: ViewContainerRef;
+  subscription!: Subscription;
+
   selectedProcess: string | null = null;
   selectedDocument: string | null = null;
 
@@ -40,6 +49,8 @@ export class ManagePlantillasCorreosComponent {
       description: 'abc............',
     },
   ];
+
+  constructor(private dialogService: DialogService) {}
 
   processOptions() {
     return [
@@ -81,5 +92,42 @@ export class ManagePlantillasCorreosComponent {
 
   setSelectedDocument(document: { id: number; text: string }) {
     this.selectedDocument = document.text;
+  }
+
+  onDeleteEmailTemplateModal(task: Process, $event: Event) {
+    $event.preventDefault();
+
+    const dialogData = new DialogData();
+    dialogData.title = 'Eliminar plantilla de correo';
+    dialogData.body = `¿Está seguro que desea eliminar la plantilla? Recuerde que la acción es irreversible.`;
+    dialogData.textButtonCancel = 'Cancelar';
+    dialogData.type = DialogType.warning;
+
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        if (dialogAction.action === ActionType.confirm) {
+          const body = `Plantilla eliminada de forma exitosa.`;
+          dialogAction.eventClose.emit();
+          this.showSuccessTaskInitializationAlertState(body);
+        } else {
+          dialogAction.eventClose.emit();
+        }
+      });
+  }
+
+  showSuccessTaskInitializationAlertState(body: string) {
+    const dialogData = new DialogData();
+    dialogData.title = body;
+    dialogData.type = DialogType.success;
+    dialogData.buttonConfirm = false;
+    dialogData.textButtonCancel = 'Cerrar';
+
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        dialogAction.eventClose.emit();
+        location.reload();
+      });
   }
 }
