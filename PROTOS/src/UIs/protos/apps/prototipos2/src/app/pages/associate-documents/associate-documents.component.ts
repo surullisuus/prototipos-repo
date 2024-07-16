43e18cf0221a5c5e3e5c1312840cf5bef6b472ff
Zheone9 <jorgito.2703@gmail.com>
2024-07-16 10:32:23 -0500

    se agregan fixes sugeridos

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/associate-documents/associate-documents.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/associate-documents/associate-documents.component.ts
@@ -1,42 +1,72 @@
-import {  Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Subscription } from 'rxjs';
+import { DialogService } from '../../components/dialog/services/dialog.service';
+import { DialogType } from '../../components/dialog/models/dialog-type';
+import { DialogData } from '../../components/dialog/models/dialog-data';
+import { DialogAction } from '../../components/dialog/models/dialog-action';
 
 @Component({
-    selector: 'app-associate-documents',
-    templateUrl: './associate-documents.component.html',
-    styleUrl: './associate-documents.component.css',
+  selector: 'app-associate-documents',
+  templateUrl: './associate-documents.component.html',
+  styleUrl: './associate-documents.component.css',
 })
 export class AssociateDocumentsComponent {
-    public data = [
-        {
-          id: 1,
-          numeroRadicado: '234',
-          fechaRadicacion: '01/12/2022',
-          seleccionado: false,
-        },
-        {
-          id: 2,
-          numeroRadicado: '235',
-          fechaRadicacion: '01/12/2022',
-          seleccionado: false,
-        },
-      ];
-      subscription!: Subscription;
-      formQueryScheme!: FormGroup;
+  @ViewChild('dialog', { read: ViewContainerRef }) dialog!: ViewContainerRef;
+  public data = [
+    {
+      id: 1,
+      numeroRadicado: '234',
+      fechaRadicacion: '01/12/2022',
+      seleccionado: false,
+    },
+    {
+      id: 2,
+      numeroRadicado: '235',
+      fechaRadicacion: '01/12/2022',
+      seleccionado: false,
+    },
+  ];
+  subscription!: Subscription;
+  formQueryScheme!: FormGroup;
 
-      constructor(private readonly fb: FormBuilder) {}
+  constructor(
+    private readonly fb: FormBuilder,
+    private dialogService: DialogService
+  ) {}
 
-      ngOnInit(): void {
-        this.formQueryScheme = this.initForm();
-      }
+  ngOnInit(): void {
+    this.formQueryScheme = this.initForm();
+  }
 
-      initForm(): FormGroup {
-        return this.fb.group({
-          keyword: [null],
-          status: [null],
-          date: [null],
-        });
-      }
+  initForm(): FormGroup {
+    return this.fb.group({
+      keyword: [null],
+      status: [null],
+      date: [null],
+    });
+  }
 
- }
+  onAssociateDocuments() {
+    //si la asociacion fue exitosa mostrar alerta de exito
+    this.showAlertState(
+      'Documentos asociados exitosamente',
+      DialogType.success
+    );
+  }
+
+  showAlertState(body: string, dialogType: DialogType) {
+    const dialogData = new DialogData();
+    dialogData.title = body;
+    dialogData.type = dialogType;
+    dialogData.buttonConfirm = false;
+    dialogData.textButtonCancel = 'Cerrar';
+
+    this.subscription = this.dialogService
+      .openModal(this.dialog, dialogData)
+      .subscribe((dialogAction: DialogAction) => {
+        location.reload();
+        dialogAction.eventClose.emit();
+      });
+  }
+}
