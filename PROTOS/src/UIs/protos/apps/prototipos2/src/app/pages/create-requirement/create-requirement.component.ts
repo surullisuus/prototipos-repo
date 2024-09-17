af12517f504f03b018cf9da7c72b5baf768946fc
surullisuus <jsfgc@hotmail.com>
2024-09-17 11:19:12 -0500

    fix: CU 77, crear y editar requisito de condigurar requisito etapa

--- PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/create-requirement/create-requirement.component.ts
+++ PROTOS/src/UIs/protos/apps/prototipos2/src/app/pages/create-requirement/create-requirement.component.ts
@@ -4,6 +4,13 @@ import { DialogAction } from '../../components/dialog/models/dialog-action';
 import { DialogType } from '../../components/dialog/models/dialog-type';
 import { DialogService } from '../../components/dialog/services/dialog.service';
 import { Subscription } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface QuerySelect {
+  text: string;
+  value: number;
+}
+
 
 @Component({
   selector: 'app-create-requirement',
@@ -17,12 +24,49 @@ export class CreateRequirementComponent {
   @ViewChild('openbuttonclose') openbuttonclose!: ElementRef;
 
 
-  constructor(private dialogService: DialogService) {  }
+  constructor(
+    private readonly fb: FormBuilder,
+    private dialogService: DialogService) {  }
 
 
   subscription!: Subscription;
+  formQueryScheme!: FormGroup;
+  typesListProcess: QuerySelect[] = [];
+  typesListStage: QuerySelect[] = [];
 
+
+  ngOnInit(): void {
+    this.formQueryScheme = this.initForm();
+   
+  }
+
+  initForm(): FormGroup {
+    this.initList()
+    return this.fb.group({
+      proceso: ["2", Validators.required],  // Campo requerido
+      etapa: ["2", Validators.required],    // Campo requerido
+      requisito: [null, Validators.required]  // Campo requerido
+       });
+    
+  }
   
+  initList(){
+    this.typesListProcess = [
+      { text: 'Proceso 1', value: 1 },
+      { text: 'Proceso 2', value: 2 },
+      { text: 'Proceso 3', value: 3 },
+      { text: 'Proceso 4', value: 4 }
+    ];
+  
+    this.typesListStage = [
+      { text: 'Etapa uno', value: 1 },
+      { text: 'Etapa dos', value: 2 },
+      { text: 'Etapa tres', value: 3 },
+      { text: 'Etapa cuatro', value: 4 }
+    ];
+  
+  }
+
   showSuccessAlert() {
     const dialogData = new DialogData();
     dialogData.title = "El requisito fue creado con éxito";
